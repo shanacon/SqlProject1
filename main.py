8afed1e8ca61910b2bec651d02b14aecbea3d892
--global <weston123741852@gmail.com>
2021-11-02 17:27:20 +0800

    add input field and label

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import tkinter as tk
+from typing import Text
 window = tk.Tk()
+Type_Input = tk.Entry(window)
+Type_Input.place(x = 100, y = 0)
+Type_Label = tk.Label(window, text = "Input Type:")
+Type_Label.place(x = 0, y = 0)
 window.title('FileTagger')
 window.geometry('800x600')
 window.mainloop()
