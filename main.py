9eea6a32038dfbf97881dedf74b25ce286ed2d9a
--global <weston123741852@gmail.com>
2021-11-03 16:36:55 +0800

    Set GUI and database

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import tkinter as tk
-from typing import Text
+import tkinter.font as font
 window = tk.Tk()
-Type_Input = tk.Entry(window)
-Type_Input.place(x = 100, y = 0)
-Type_Label = tk.Label(window, text = "Input Type:")
-Type_Label.place(x = 0, y = 0)
+###
+BtnFont = font.Font(size=20)
+EntryFont = font.Font(size=14)
+###
+New_Type_Btn = tk.Button(window, text ="New Type", bg = "light blue", width = '10', height = '1', font = BtnFont)
+New_Type_Btn.place(x = 50, y = 50)
+New_Tag_Btn = tk.Button(window, text ="New Tag", bg = "light blue", width = '10', height = '1', font = BtnFont)
+New_Tag_Btn.place(x = 50, y = 100)
+Browse_Btn = tk.Button(window, text ="Browse", bg = "light blue", width = '10', height = '1', font = BtnFont)
+Browse_Btn.place(x = 50, y = 175)
+Add_Tag_Btn = tk.Button(window, text ="Add Tag", bg = "light blue", width = '10', height = '1', font = BtnFont)
+Add_Tag_Btn.place(x = 210, y = 175)
+Save_Btn = tk.Button(window, text ="Save", bg = "light blue", width = '10', height = '1', font = BtnFont)
+Save_Btn.place(x = 370, y = 175)
+###
+file_path = tk.Entry(window, font = EntryFont, width = '52')
+file_path.insert(0, 'input path')
+file_path.place(x = 50, y = 150)
 window.title('FileTagger')
-window.geometry('800x600')
+window.geometry('800x300')
 window.mainloop()
