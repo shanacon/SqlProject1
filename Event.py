286871ef843a9cfe8c081b69e9ca5b7343756317
shanacon <weston123741852@gmail.com>
2021-11-06 20:55:03 +0800

    almost done

--- Event.py
+++ Event.py
@@ -1,29 +1,35 @@
 import sqlite3 as lite
 import tkinter as tk
+import os
+from tkinter import filedialog
 ###
-def NewType(Type_Input, DropDown, var) :
+def NewType(Type_Input, DropDown_1, var_1, DropDown_2, var_2, StatusText) :
     con = lite.connect('FileData.db')
     if Type_Input.get()=="":
-        print("todo : status Text")
+        StatusText['text'] = "Please input Type name"
     else :
         with con:
             cur=con.cursor()
-            cur.execute(f"Insert into Type Values(null, '{Type_Input.get()}')")
+            cur.execute(f"INSERT into Type Values(null, '{Type_Input.get()}')")
     Type_Input.delete(0, 'end')
-    return RefreshDropDown(DropDown, var)
+    return RefreshTypeList(DropDown_1, var_1, DropDown_2, var_2)
 ###
-def RefreshDropDown(DropDown, var):
-    DropDown['menu'].delete(0, 'end')
+def RefreshTypeList(DropDown_1, var_1, DropDown_2, var_2):
+    DropDown_1['menu'].delete(0, 'end')
+    DropDown_2['menu'].delete(0, 'end')
     con = lite.connect('FileData.db')
     cur=con.cursor()
     TypeList = []
-    for item in cur.execute ("select name from Type") :
+    for item in cur.execute ("SELECT name from Type") :
         TypeList.append(item[0])
     for item in TypeList: 
-        DropDown['menu'].add_command(label=item, command=tk._setit(var, item))
+        DropDown_1['menu'].add_command(label=item, command=tk._setit(var_1, item))
+        DropDown_2['menu'].add_command(label=item, command=tk._setit(var_2, item))
+    if TypeList :
+        var_1.set(TypeList[0])
     return TypeList
 ###
-def NewTag(Tag_Input, variable):
+def NewTag(Tag_Input, variable, StatusText):
     con = lite.connect('FileData.db')
     with con:
         cur=con.cursor()
@@ -31,9 +37,164 @@ def NewTag(Tag_Input, variable):
             if variable.get() != "":
                 cur.execute(f"SELECT type_id from Type WHERE name='{variable.get()}'")
                 id = cur.fetchone()[0]
-                cur.execute(f"Insert into Tag Values(null, '{Tag_Input.get()}', '{id}')")
+                cur.execute(f"INSERT into Tag Values(null, '{Tag_Input.get()}', '{id}')")
                 Tag_Input.delete(0, 'end')
             else:
-                print("todo : status Text")
+                StatusText['text'] = "Please select Type"
         else:
-            print("todo : status Text")
+            StatusText['text'] = "Please input Tag"
+###
+def Browse(file_path, StatusText):
+    file_path.delete(0, 'end')
+    filename = filedialog.askdirectory(initialdir = os.getcwd(),title = "Select a File")
+    if filename == None or filename == "":
+       StatusText['text'] = "please choose folder"
+       return None
+    file_path.insert(0, filename)
+    return Check_Path(filename)
+###
+def Check_Path(path) :
+    if path == "" :
+        return
+    con = lite.connect('FileData.db')
+    cur=con.cursor()
+    cur.execute (f"SELECT file_id from File WHERE path = '{path}'")
+    tmp = cur.fetchone()
+    if tmp == None :
+        file_id = InsertPath(path)
+    else :
+        file_id = tmp[0]
+    return file_id
+###
+def InsertPath(path):
+    con = lite.connect('FileData.db')
+    with con:
+        cur=con.cursor()
+        cur.execute(f"Insert into File Values(null, '{path}', '{os.path.basename(path)}')")
+        cur.execute(f"SELECT file_id from File WHERE path = '{path}'")
+        return cur.fetchone()[0]
+###
+def SetFileTag(file_id,tag_id):
+    con = lite.connect('FileData.db')
+    with con:
+        cur=con.cursor()
+        cur.execute(f"Insert into Relation Values('{file_id}', '{tag_id}')")
+##
+def RefreshTagSystem(DropDown_1, var_1, type):
+    DropDown_1['menu'].delete(0, 'end')
+    con = lite.connect('FileData.db')
+    cur=con.cursor()
+    cur.execute (f"SELECT type_id from Type WHERE name = '{type}'")
+    type = cur.fetchone()[0]
+    TagList = []
+    for item in cur.execute(f"SELECT name from Tag WHERE type = {type}") :
+        TagList.append(item[0])
+    for item in TagList: 
+        DropDown_1['menu'].add_command(label=item, command=tk._setit(var_1, item))
+    if TagList :
+        var_1.set(TagList[0])
+###
+def DeleteRelation(file_id, tag_id) :
+    con = lite.connect('FileData.db')
+    cur=con.cursor()
+    cur.execute(f"Delete from Relation WHERE file_id = {file_id} AND tag_id = {tag_id}")
+    con.commit()
+###
+def GetTagsByFileID(file_id) :
+    con = lite.connect('FileData.db')
+    cur=con.cursor()
+    TagList = []
+    for tag_id in cur.execute(f"SELECT tag_id from Relation WHERE file_id = {file_id}") :
+        TagList.append(tag_id[0])
+    return TagList
+###
+def GetPos(count, item) :
+    item.place(x = 540 + (count%2) * 130, y = 15 + int(count/2) * 35)
+def GetPosOfSearch(count, item) :
+    item.place(x = 40, y = 15 + count * 35)
+###
+def GetTagidByName(TagName, StatusText):
+    con = lite.connect('FileData.db')
+    cur=con.cursor()
+    cur.execute(f"SELECT tag_id from Tag WHERE name = '{TagName}'")
+    tmp = cur.fetchone()
+    if tmp :
+        return tmp[0]
+    else :
+        StatusText['text'] = "no such tag"
+def GetFileidByPath(path, StatusText):
+    if path == None or path == "input path" :
+        StatusText['text'] = "no such file"
+    else :
+        return -1
+    con = lite.connect('FileData.db')
+    cur=con.cursor()
+    cur.execute (f"SELECT file_id from File WHERE path = '{path}'")
+    tmp = cur.fetchone()
+    if tmp :
+        return tmp[0]
+    else :
+        StatusText['text'] = "no such file"
+def GetNameByTagid(TagID, StatusText):
+    con = lite.connect('FileData.db')
+    cur=con.cursor()
+    cur.execute(f"SELECT name from Tag WHERE tag_id = {TagID}")
+    tmp = cur.fetchone()
+    if tmp :
+        return tmp[0]
+    else :
+        StatusText['text'] = "no such tag id"
+def GetPathByFileid(fileID, StatusText):
+    con = lite.connect('FileData.db')
+    cur=con.cursor()
+    cur.execute (f"SELECT path from File WHERE file_id = '{fileID}'")
+    tmp = cur.fetchone()
+    if tmp :
+        return tmp[0]
+    else :
+        StatusText['text'] = "no such fileid"
+####
+def SearchByString(input, StatusText) :
+    tagList = []
+    LogicList = []
+    ret = []
+    if input == None or input == "" :
+        con = lite.connect('FileData.db')
+        cur=con.cursor()
+        command = "SELECT DISTINCT  file_id FROM Relation"
+        cur.execute(command)
+        tmp = cur.fetchall()
+        if tmp :
+            for item in tmp :
+                ret.append(item[0])
+        else :
+            StatusText['text'] = "No file Found"
+        return ret
+    And_1 = "SELECT file_id FROM(SELECT * FROM Relation WHERE file_id IN("
+    ## succuess :"SELECT file_id FROM(SELECT * FROM Relation WHERE file_id IN(SELECT file_id FROM Relation WHERE tag_id = 8)) as a WHERE tag_id = 10"
+    tmp = input.split(' ')
+    for str in tmp :
+        if len(tagList) == len(LogicList) :
+            tagList.append(str)
+        else :
+            LogicList.append(str)
+    command = f"SELECT file_id FROM Relation WHERE tag_id = {GetTagidByName(tagList[0], StatusText)}"
+    count = 1
+    for logic in LogicList :
+        if logic == "and":
+            command = And_1 + command + f")) as a WHERE tag_id = {GetTagidByName(tagList[count], StatusText)}"
+        elif logic == "or":
+            orcmd = f"SELECT file_id FROM Relation WHERE tag_id = {GetTagidByName(tagList[count], StatusText)}"
+            command = command + " UNION " + orcmd
+        count+=1
+    con = lite.connect('FileData.db')
+    cur=con.cursor()
+    # print(command)
+    cur.execute(command)
+    tmp = cur.fetchall()
+    if tmp :
+        for item in tmp :
+            ret.append(item[0])
+    else :
+        StatusText['text'] = "No file Found"
+    return ret
--- main.py
+++ main.py
@@ -8,8 +8,74 @@ EntryFont = font.Font(size=14)
 ###
 TypeList = []
 TagList = []
-
-###
+TagBtnList = []
+Now_fileID = -1
+### function
+def AddTagByDropDown() :
+    tag_id = GetTagidByName(tag_var.get(), StatusText)
+    file_id = GetFileidByPath(file_path.get(), StatusText)
+    if file_id != -1 :
+        SetFileTag(file_id, tag_id)
+        SetTagsShow()
+def AddTagByEntry() :
+    tag_id = GetTagidByName(Tag_E.get(), StatusText)
+    file_id = GetFileidByPath(file_path.get(), StatusText)
+    Tag_E.delete(0, 'end')
+    if file_id != -1 :
+        SetFileTag(file_id, tag_id)
+        SetTagsShow()
+def SetTagsShow() :
+    RemoveAllTagBtn()
+    FileTagList = GetTagsByFileID(Now_fileID)
+    count = 0
+    for id in FileTagList :
+        newBtn = tk.Button(window, text = GetNameByTagid(id, StatusText), bg = "antique white", width = '10', height = '1', font = EntryFont)
+        newBtn.config(command = lambda id = id :_DeleteRelation(Now_fileID, id))
+        GetPos(count, newBtn)
+        TagBtnList.append(newBtn)
+        count += 1
+def RemoveAllTagBtn() :
+    global TagBtnList
+    for Btn in TagBtnList :
+        Btn.destroy()
+    TagBtnList.clear()
+def SetSearchResultWindow(PathList) :
+    SearchResult = tk.Toplevel(window)
+    SearchResult.geometry('200x500')
+    ###
+    count = 0
+    for path in PathList :
+        newBtn = tk.Button(SearchResult, text = os.path.basename(path), bg = "antique white", width = '10', height = '1', font = EntryFont)
+        newBtn.config(command = lambda path = path :OpenExploer(path))
+        GetPosOfSearch(count, newBtn)
+        count += 1
+def OpenExploer(path):
+    os.startfile(path)
+def _NewTag() :
+    NewTag(Tag_Input, variable, StatusText)
+    RefreshTagSystem(tag_d, tag_var, variable.get())
+def _Browse() :
+    global Now_fileID
+    Now_fileID = Browse(file_path, StatusText)
+    if Now_fileID != None :
+        SetTagsShow()
+def _Check_Path() :
+    global Now_fileID
+    Now_fileID = Check_Path(file_path.get())
+    SetTagsShow()
+def _DeleteRelation(file_id, tag_id) :
+    DeleteRelation(file_id, tag_id)
+    SetTagsShow()
+def _SearchByString() :
+    FileidList = SearchByString(Search_Input.get(), StatusText)
+    PathList = []
+    for id in FileidList :
+        PathList.append(GetPathByFileid(id), StatusText)
+    SetSearchResultWindow(PathList)
+### Label
+StatusText = tk.Label(window, text ="", font = EntryFont)
+StatusText.place(x = 380, y = 60)
+### button
 New_Type_Btn = tk.Button(window, text ="New Type", bg = "light blue", width = '10', height = '1', font = BtnFont)
 New_Type_Btn.place(x = 50, y = 50)
 New_Tag_Btn = tk.Button(window, text ="New Tag", bg = "light blue", width = '10', height = '1', font = BtnFont)
@@ -17,28 +83,52 @@ New_Tag_Btn.place(x = 50, y = 100)
 Browse_Btn = tk.Button(window, text ="Browse", bg = "light blue", width = '10', height = '1', font = BtnFont)
 Browse_Btn.place(x = 50, y = 175)
 Add_Tag_Btn = tk.Button(window, text ="Add Tag", bg = "light blue", width = '10', height = '1', font = BtnFont)
-Add_Tag_Btn.place(x = 210, y = 175)
-Save_Btn = tk.Button(window, text ="Save", bg = "light blue", width = '10', height = '1', font = BtnFont)
-Save_Btn.place(x = 370, y = 175)
-###
+Add_Tag_Btn.place(x = 370, y = 190)
+Check_Btn = tk.Button(window, text ="Check", bg = "light blue", width = '10', height = '1', font = EntryFont)
+Check_Btn.place(x = 410, y = 142)
+Add_Tag_Btn2 = tk.Button(window, text ="Add Tag", bg = "light blue", width = '10', height = '1', font = EntryFont)
+Add_Tag_Btn2.place(x = 370, y = 260)
+Search_Btn = tk.Button(window, text ="Search", bg = "light blue", width = '10', height = '1', font = BtnFont)
+Search_Btn.place(x = 370, y = 325)
+### input entry
 Type_Input = tk.Entry(window, font = BtnFont, width = '11')
 Type_Input.place(x = 210, y = 58)
 Tag_Input = tk.Entry(window, font = BtnFont, width = '11')
 Tag_Input.place(x = 210, y = 108)
-file_path = tk.Entry(window, font = EntryFont, width = '52')
+file_path = tk.Entry(window, font = EntryFont, width = '38')
 file_path.insert(0, 'input path')
 file_path.place(x = 50, y = 150)
-###
+Tag_E = tk.Entry(window, font = EntryFont, width = '16')
+Tag_E.insert(0, 'input tag')
+Tag_E.place(x = 210, y = 265)
+Search_Input = tk.Entry(window, font = EntryFont, width = '34')
+Search_Input.place(x = 50, y = 337)
+### dropdown
 variable = tk.StringVar(window)
 type_of_tag = tk.OptionMenu(window, variable, "")
 type_of_tag.config(width = 11, bg = "light blue", font = EntryFont)
-type_of_tag.place(x = 370, y = 108)
+type_of_tag.place(x = 378, y = 108)
+#
+type_var = tk.StringVar(window)
+type_d = tk.OptionMenu(window, type_var, "")
+type_d.config(width = 12, bg = "light blue", font = EntryFont)
+type_d.place(x = 208, y = 175)
+tag_var = tk.StringVar(window)
+tag_d = tk.OptionMenu(window, tag_var, "")
+tag_d.config(width = 12, bg = "light blue", font = EntryFont)
+tag_d.place(x = 208, y = 215)
 ### set command
-TypeList = RefreshDropDown(type_of_tag, variable)
-if TypeList :
-    variable.set(TypeList[0])
-New_Type_Btn.config(command = lambda:NewType(Type_Input, type_of_tag, variable))
-New_Tag_Btn.config(command = lambda:NewTag(Tag_Input, variable))
+TypeList = RefreshTypeList(type_of_tag, variable, type_d, type_var)
+New_Type_Btn.config(command = lambda:NewType(Type_Input, type_of_tag, variable, type_d, type_var, StatusText))
+New_Tag_Btn.config(command = lambda:_NewTag())
+Browse_Btn.config(command = lambda:_Browse())
+Check_Btn.config(command = lambda:_Check_Path())
+type_var.trace("w", lambda *args: RefreshTagSystem(tag_d, tag_var, type_var.get()))
+Add_Tag_Btn.config(command = lambda:AddTagByDropDown())
+Add_Tag_Btn2.config(command = lambda:AddTagByEntry())
+Search_Btn.config(command = lambda:_SearchByString())
+###
 window.title('FileTagger')
-window.geometry('800x300')
+window.geometry('800x400')
 window.mainloop()
+###
